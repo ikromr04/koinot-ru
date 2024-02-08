479fb6a51d52d9761f1e2b83e2e948d1ced1d51c
ikromr04 <ikromr04@gmail.com>
2024-02-08 11:33:04 +0500

    Adds google recaptcha v3

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -32,47 +32,7 @@
 
   <link rel="stylesheet" href="{{ asset('lightbox/lc_lightbox.css') }}">
   <link rel="stylesheet" href="{{ mix('css/style.min.css') }}">
-
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123986695-68"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-
-    gtag('config', 'UA-123986695-68');
-  </script>
-
-  <!-- Yandex.Metrika counter -->
-  <script type="text/javascript">
-    (function(m, e, t, r, i, k, a) {
-      m[i] = m[i] || function() {
-        (m[i].a = m[i].a || []).push(arguments)
-      };
-      m[i].l = 1 * new Date();
-      for (var j = 0; j < document.scripts.length; j++) {
-        if (document.scripts[j].src === r) {
-          return;
-        }
-      }
-      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-    })
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(90880837, "init", {
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-      webvisor: true
-    });
-  </script>
-  <noscript>
-    <div><img src="https://mc.yandex.ru/watch/90880837" style="position:absolute; left:-9999px;" alt="" /></div>
-  </noscript>
-  <!-- /Yandex.Metrika counter -->
+  {!! htmlScriptTagJsApi() !!}
 </head>
 
 <body class="page__body">
